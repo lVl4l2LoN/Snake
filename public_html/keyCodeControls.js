a9c2c6ea0ce6e381242995353078053a4a0d6d4f
Marlon <m.c4n1as@gmail.com>
2018-03-23 19:39:42 -0700

    keyCodeControls
    Added a new javascript file called keyCodeControls. This file allows the snake movement by pressing the arrow keys on the keyboard.

---
+++ public_html/keyCodeControls.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//keyCode controls to allow the snake to move up, down, left and right
+//by using the keyboard arrow keys (the values are 37, 38, 39,40). The function will allow the movement while
+//making sure that the snake can't go to the opposite direction while travelling
+//at a certain direction to avoid immediate collision with itself.
+
+(function(window,document,drawModule,undefined){
+    //connect the button in the html with the init function
+    var btn = document.getElementById('btn');
+    btn.addEventListener("click", function(){
+        drawModule.init();
+    });
+    
+    document.onkeydown = function(event){
+        
+        keyCode = window.event.keyCode;
+        keyCode = event.keyCode;
+        
+        switch(keyCode){
+            
+            case 37:
+                if(direction !='right'){
+                    direction = 'left';
+                }
+                console.log('left');
+                break;
+            case 39:
+                if(direction != 'left'){
+                    direction = 'right';
+                    console.log('right');
+                }
+                break;
+            case 38:
+                if(direction !='down'){
+                    direction = 'up';
+                    console.log('up');
+                }
+                break;
+            case 40:
+                if(direction !='up'){
+                    direction = 'down';
+                    console.log('down');
+                }
+                break;
+        }
+    }
+})(window, document,drawModule);
+
+
